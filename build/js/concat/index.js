184d83ec8b6b0546343267290fb1ffe62cbbf2ac
Viengo <viengooleg@gmail.com>
2015-09-10 12:38:23 +0300

    footer

--- build/js/concat/index.js
+++ build/js/concat/index.js
@@ -17,3 +17,33 @@ $(function () {
         }
     });
 });
+
+$(function () {
+var $clientcarousel = $('#clients-list');
+var clients = $clientcarousel.children().length;
+var clientwidth = (clients * 425); // 140px width for each client item
+$clientcarousel.css('width',clientwidth);
+
+var rotating = true;
+var clientspeed = 3000;
+var seeclients = setInterval(rotateClients, clientspeed);
+
+$(document).on({
+    mouseenter: function(){
+        rotating = false; // turn off rotation when hovering
+    },
+    mouseleave: function(){
+        rotating = true;
+    }
+}, '#clients');
+
+function rotateClients() {
+    if(rotating != false) {
+        var $first = $('#clients-list li:first');
+        $first.animate({ 'margin-left': '-425px' }, 600, function() {
+            $first.remove().css({ 'margin-left': '100px' });
+            $('#clients-list li:last').after($first);
+        });
+    }
+}
+});
